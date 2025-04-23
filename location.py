80b3e0263c75d9969b37f5ae9b00cb781f3f14ae
VvV <vaivasvat.verma@gmail.com>
2025-04-23 11:50:28 +0530

    Update location.py

--- location.py
+++ location.py
@@ -1,18 +1,21 @@
 from geopy.geocoders import Nominatim
 
-geolocator = Nominatim(user_agent="your_app_name")  # Replace "your_app_name"
+geolocator = Nominatim(user_agent="SongBird") 
 
 def get_airport_coordinates(airport_name):
+
     location = geolocator.geocode(airport_name)
     if location:
         return (location.latitude, location.longitude)
     else:
         return None
 
-airport_name = "KLAX"
-coordinates = get_airport_coordinates(airport_name)
+def Location(ID):
+    
+    airport_name1 = ID
+    coordinates = get_airport_coordinates(airport_name1)
 
-if coordinates:
-    print(f"The coordinates for {airport_name} are: Latitude {coordinates[0]}, Longitude {coordinates[1]}")
-else:
-    print(f"Could not find coordinates for {airport_name}")
+    if coordinates:
+        return coordinates
+    else:
+        print(f"Could not find coordinates")
