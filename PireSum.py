9937ed58dc4182e9967302ee5f3e48c06e61662b
VvV <vaivasvat.verma@gmail.com>
2025-04-23 11:48:47 +0530

    Update PireSum.py

--- PireSum.py
+++ PireSum.py
@@ -5,19 +5,24 @@ from UTC import utc
 
 def Piresum(IDs):
     summary=[]
+    raw={}
     for ID in IDs:
-        new=Pireps("KLAX")
+        new=Pireps(ID)
+        pireps={}
         time=utc("Metar")
         counters = defaultdict(int)
         x="https://aviationweather.gov/api/data/pirep?id="+ID+"&format=raw&age=1&distance=1000&level=10000&inten=sev&date="+time
         req=requests.get(x)
         pirep_lines = req.text.strip().split('\n')
-
+        counter=0
         for line in pirep_lines:
-            
+            if "TOP" in line and "T" in line:  # crude filter
+                continue
             Pireps.parse(new,line)
-
             data=new.data
+            counter += 1
+            pireps[counter]=data
+         
             if data == None:
                 pass
             else:
@@ -34,12 +39,11 @@ def Piresum(IDs):
             
 
         counters=dict(counters)
-    
+        
         output = f"{ID}: " + ", ".join(f"{k}={v}" for k, v in counters.items())
         summary.append(output)
-    return "\n\n".join(summary)
-
-
-IDs=["KLAX","KJFK","KPHX"]
-print(Piresum(IDs))
+        pireps["status"]=output
+        raw[ID]=pireps
+    summary="\n\n".join(summary)
+    return raw,summary
 
